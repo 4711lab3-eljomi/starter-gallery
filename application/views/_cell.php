db11c02b3d9eb68e4c15545d25f5f26ccc1258bc
Mike Zhou <mike.weizhou@gmail.com>
2018-01-23 21:22:43 -0800

    Basic CI to Good CI Website ConversioN
    Signed-off-by: Mike Zhou <mike.weizhou@gmail.com>

---
+++ application/views/_cell.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+The original HTML:
+
+            <a href="./data/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg" data-lightbox="gallery" data-title="Caledonian orogeny fold in King Oscar Fjord ... Posted 2014.05.04 by mickey, in landscape"><img src="./data/thumb/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg"/></a>
+
+Converted to be a view fragment template.
+*/
+
+?>
+
+<a href="./data/{filename}" data-lightbox="gallery" data-title="{title} ... Posted {uploaded} by {uploader}, in {category}">
+	<img src="./data/thumb/{filename}"/>
+</a>
